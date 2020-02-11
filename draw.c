7336e8224c6c501927d58c3f0392f2052e582b58
DarthChurros <tnarayan00@stuy.edu>
2020-02-11 11:15:55 -0500

    wrote write_image

--- draw.c
+++ draw.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "draw.h"
 
 void init_pixel(struct pixel pix) {
@@ -12,4 +14,23 @@ void draw_line(struct pixel** img_ary) {
 
 void init_image(struct pixel** img_ary) {
 
+}
+
+void write_image(struct pixel** img_ary) {
+  FILE* img = fopen("image.ppm", "w");
+  fprintf(img, "P3\n512 512 255\n");
+
+  int i;
+  int j;
+  int k;
+  for (i = 0; i < 512; i++) {
+    for (j = 0; j < 512; j++) {
+      fprintf(img, "%d ", img_ary[i][j].r));
+      fprintf(img, "%d ", img_ary[i][j].g));
+      fprintf(img, "%d ", img_ary[i][j].b));
+    }
+    fprintf(img, "\n");
+  }
+
+  fclose(img);
 }
--- draw.h
+++ draw.h
@@ -10,6 +10,7 @@ struct pixel {
 void init_pixel(struct pixel pix);
 void draw_line(struct pixel** img_ary);
 void init_image(struct pixel** img_ary);
+void write_image(struct pixel** img_ary);
 
 
 #endif
